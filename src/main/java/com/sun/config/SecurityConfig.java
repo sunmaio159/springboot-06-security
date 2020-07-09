310d5780502544afb29559b7e0903e26e9e6a35b
sunmaio159 <sunmiao159@gmail.com>
2020-07-09 23:30:18 +0800

    cinfig提交

---
+++ src/main/java/com/sun/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.sun.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //首页所有人可以访问，功能页只有对应有权限的人才能访问
+        //请求授权的规则
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+        //没有权限默认会到登录页面，需要开启登录的页面
+        http.formLogin();
+    }
+
+    //认证
+    //密码编码：PasswordEncoder
+    //在spring Security 5.0+ 新增了很多的加密方法
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //这些数据正常从数据库中读
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("sun").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2","vip3")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1");
+    }
+}
